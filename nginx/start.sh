a59fc9fe57e0500a1bd7cdfcf1feed26be684915
Known_Black_Hat <knownblackhat@gmail.com>
2023-01-17 17:11:52 +0530

    Priority Problem Resolved -> Contanarization problem

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,3 +1,5 @@
-nginx
-tail -f /var/log/nginx/access.log
+#!/bin/bash
+systemctl start nginx
+echo 'IP:' $(ip a s eth0 | grep  -w 'inet' | cut -d '/' -f 1 | tr 'inet' ' ')
+tail -f /var/log/nginx/access.log /var/log/nginx/error.log 
 
